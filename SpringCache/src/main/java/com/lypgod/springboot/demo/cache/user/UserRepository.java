e28954c1b2c5cc330b4b9f960422defc82513957
lypgod <lypgod@hotmail.com>
2019-09-16 15:13:13 +0800

    msg

--- SpringCache/src/main/java/com/lypgod/springboot/demo/cache/user/UserRepository.java
+++ SpringCache/src/main/java/com/lypgod/springboot/demo/cache/user/UserRepository.java
@@ -1,9 +1,11 @@
 package com.lypgod.springboot.demo.cache.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author lypgod
  */
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
